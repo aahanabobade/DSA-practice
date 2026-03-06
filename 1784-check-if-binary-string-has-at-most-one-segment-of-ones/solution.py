a93148480c5e13e40e838d100612a8458b6c1f34
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-06 16:53:52 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/solution.py
@@ -0,0 +1,16 @@
+# Check if Binary String Has at Most One Segment of Ones
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        if "01" in s:
+            return False
+        else:
+            return True            
+        
