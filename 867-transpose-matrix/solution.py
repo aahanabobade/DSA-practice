578577dac3f31479997e0d590bf203e7f55b0413
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-07 23:49:07 +0530

    Time: 0 ms | Memory: 12.9 MB - LeetPush

---
+++ 867-transpose-matrix/solution.py
@@ -0,0 +1,18 @@
+# Transpose Matrix
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.9 MB
+# https://leetcode.com/problems/transpose-matrix/
+
+            row = []
+            for i in range(m):
+                row.append(matrix[i][j])
+            result.append(row)
+            
+        for j in range(n):
+        
+        
+        result = []
+        m = len(matrix)
+        n = len(matrix[0])
+        """
