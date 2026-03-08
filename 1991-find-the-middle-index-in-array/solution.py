ac787d6a7b87ea5df971e801ffa7a61a34cb4bf8
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-08 07:22:52 +0530

    Time: 1 ms | Memory: 12.4 MB - LeetPush

---
+++ 1991-find-the-middle-index-in-array/solution.py
@@ -0,0 +1,17 @@
+# Find the Middle Index in Array
+# Difficulty: Easy
+# Runtime: 1 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/find-the-middle-index-in-array/
+
+        for i in range(n):
+        
+        left = 0
+        n = len(nums)
+        total = 0
+        """
+        :rtype: int
+        :type nums: List[int]
+        """
+    def findMiddleIndex(self, nums):
+class Solution(object):
