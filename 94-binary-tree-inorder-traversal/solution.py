365e0a8a5ba4d9de67da978647f9cbaeba1cfa15
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-12 20:59:27 +0530

    Time: 0 ms | Memory: 12.3 MB - LeetPush

---
+++ 94-binary-tree-inorder-traversal/solution.py
@@ -0,0 +1,18 @@
+# Binary Tree Inorder Traversal
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.3 MB
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+        res = []
+        """
+        :rtype: List[int]
+        :type root: Optional[TreeNode]
+        """
+    def inorderTraversal(self, root):
+class Solution(object):
+#         self.right = right
+#         self.left = left
+#         self.val = val
+#     def __init__(self, val=0, left=None, right=None):
+# class TreeNode(object):
