f2ce2139a7fd243ba1b3c028b5fb11e8cb1f906f
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-12 20:56:40 +0530

    Time: 2848 ms | Memory: 57.8 MB - LeetPush

---
+++ 3600-maximize-spanning-tree-stability-with-upgrades/solution.py
@@ -0,0 +1,18 @@
+# Maximize Spanning Tree Stability with Upgrades
+# Difficulty: Hard
+# Runtime: 2848 ms
+# Memory: 57.8 MB
+# https://leetcode.com/problems/maximize-spanning-tree-stability-with-upgrades/
+
+    def union(self, a, b):
+
+        return self.p[x]
+            self.p[x] = self.find(self.p[x])
+        if self.p[x] != x:
+    def find(self, x):
+
+        self.r = [0]*n
+        self.p = list(range(n))
+    def __init__(self, n):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
