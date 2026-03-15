43f9aea1cc8e89bd61a2332d8449262243eba291
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-15 12:02:09 +0530

    Time: 0 ms | Memory: 13.1 MB - LeetPush

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -0,0 +1,18 @@
+# Find First and Last Position of Element in Sorted Array
+# Difficulty: Medium
+# Runtime: 0 ms
+# Memory: 13.1 MB
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+                elif nums[mid] < target:
+                    right = mid-1
+                    first = mid
+                if nums[mid]==target:
+
+                mid = (left+right)//2
+            while left<=right:
+
+            first = -1
+            right = len(nums)-1
+            left  = 0 
+        def first(nums,target):
