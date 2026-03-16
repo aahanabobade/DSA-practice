d033c80a0ea25aea3b1e62df663e74d6c62abd5d
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-16 12:40:23 +0530

    Time: 15 ms | Memory: 12.7 MB - LeetPush

---
+++ 242-valid-anagram/solution.py
@@ -0,0 +1,18 @@
+# Valid Anagram
+# Difficulty: Easy
+# Runtime: 15 ms
+# Memory: 12.7 MB
+# https://leetcode.com/problems/valid-anagram/
+
+        
+        for char in t:
+            if char not in count:
+                return False
+            else: 
+                count[char]-=1
+                count[char]=1
+            else:
+                count[char]+=1
+                if count[char] < 0:
+                    return False
+        
