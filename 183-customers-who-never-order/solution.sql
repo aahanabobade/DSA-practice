2e3dbdcf5d8aa8cbbf990b681d744ea8f74eec18
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-18 12:39:58 +0530

    Time: 587 ms | Memory: 0B - LeetPush

---
+++ 183-customers-who-never-order/solution.sql
@@ -0,0 +1,12 @@
+-- Customers Who Never Order
+-- Difficulty: Easy
+-- Runtime: 587 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/customers-who-never-order/
+
+# Write your MySQL query statement below
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL;
