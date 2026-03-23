2fec790385ca9710542f746757e0483054f69588
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-23 17:14:18 +0530

    Time: 1465 ms | Memory: 0B - LeetPush

---
+++ 607-sales-person/solution.sql
@@ -0,0 +1,17 @@
+-- Sales Person
+-- Difficulty: Easy
+-- Runtime: 1465 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/sales-person/
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT sales_id
+    FROM Orders
+    WHERE com_id = (
+        SELECT com_id
+        FROM Company
+        WHERE name = 'RED'
+    )
+);
