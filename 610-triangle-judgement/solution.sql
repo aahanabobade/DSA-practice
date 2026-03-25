63500bd28b55dcda476c939915355e6da8b1d922
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-25 17:14:38 +0530

    Time: 259 ms | Memory: 0B - LeetPush

---
+++ 610-triangle-judgement/solution.sql
@@ -0,0 +1,17 @@
+-- Triangle Judgement
+-- Difficulty: Easy
+-- Runtime: 259 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/triangle-judgement/
+
+# Write your MySQL query statement below
+SELECT 
+    x, 
+    y, 
+    z,
+    CASE 
+        WHEN x + y > z AND x + z > y AND y + z > x 
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
