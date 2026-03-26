425de28d450c93f8f8cfba846cc88dc3b502447b
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-03-26 16:38:48 +0530

    Time: 497 ms | Memory: 0B - LeetPush

---
+++ 1075-project-employees-i/solution.sql
@@ -0,0 +1,14 @@
+-- Project Employees I
+-- Difficulty: Easy
+-- Runtime: 497 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/project-employees-i/
+
+# Write your MySQL query statement below
+SELECT 
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM Project p
+JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id;
