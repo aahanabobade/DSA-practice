8c3f95871cd271ef57e1951e39f29e9b42b7f47b
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-02 17:04:27 +0530

    Time: 765 ms | Memory: 0B - LeetPush

---
+++ 1407-top-travellers/solution.sql
@@ -0,0 +1,14 @@
+-- Top Travellers
+-- Difficulty: Easy
+-- Runtime: 765 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/top-travellers/
+
+SELECT 
+    u.name,
+    IFNULL(SUM(r.distance), 0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+    ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
