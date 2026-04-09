e3c9c118e0cb22475c1a4fc7c7905581556485c8
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-09 10:20:30 +0530

    Time: 375 ms | Memory: 0B - LeetPush

--- 181-employees-earning-more-than-their-managers/solution.sql
+++ 181-employees-earning-more-than-their-managers/solution.sql
@@ -1,12 +1,12 @@
 -- Employees Earning More Than Their Managers
 -- Difficulty: Easy
--- Runtime: 344 ms
+-- Runtime: 375 ms
 -- Memory: 0B
 -- https://leetcode.com/problems/employees-earning-more-than-their-managers/
 
 # Write your MySQL query statement below
-SELECT e1.name AS Employee
-FROM Employee e1
-JOIN Employee e2
-on e1.managerId = e2.id
-where e1.salary>e2.salary
+select e.name as Employee 
+from Employee e
+join Employee m
+on e.managerId = m.id
+where e.salary>m.salary
