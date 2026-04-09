cd480385aeaad39739097993d0768f3887e8230d
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-09 10:15:18 +0530

    Time: 378 ms | Memory: 0B - LeetPush

---
+++ 175-combine-two-tables/solution.sql
@@ -0,0 +1,12 @@
+-- Combine Two Tables
+-- Difficulty: Easy
+-- Runtime: 378 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/combine-two-tables/
+
+# Write your MySQL query statement below
+select p.firstName, p.lastName,a.city,a.state
+from Person p
+LEFT JOIN Address a
+on p.personId = a.personId
+
