7f4288b76d2392b3fe02392efb238b6f5b2bcb7f
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-11 20:19:00 +0530

    Time: 9435 ms | Memory: 20.7 MB - LeetPush

---
+++ minimum-operations-to-transform-array-into-alternating-prime/solution.py
@@ -0,0 +1,7 @@
+# Q3. Minimum Operations to Transform Array into Alternating Prime
+# Difficulty: Medium
+# Runtime: 9435 ms
+# Memory: 20.7 MB
+# https://leetcode.com/problems/minimum-operations-to-transform-array-into-alternating-prime/
+
+# code not captured
