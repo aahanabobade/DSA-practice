792366ff6f9c03eae17360413a22b009e2c35a8f
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-11 12:13:18 +0530

    Time: 19 ms | Memory: 13.1 MB - LeetPush

---
+++ 495-teemo-attacking/solution.py
@@ -0,0 +1,17 @@
+# Teemo Attacking
+# Difficulty: Easy
+# Runtime: 19 ms
+# Memory: 13.1 MB
+# https://leetcode.com/problems/teemo-attacking/
+
+        :type timeSeries: List[int]
+        :type duration: int
+        :rtype: int
+        """
+        total = 0
+        n = len(timeSeries)
+
+        for i in range(n-1):
+        """
+    def findPoisonedDuration(self, timeSeries, duration):
+class Solution(object):
