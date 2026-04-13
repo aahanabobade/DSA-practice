4311da16e6fd981ffd21f320d784b8dd94882c01
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-13 14:39:58 +0530

    Time: 0 ms | Memory: 12.5 MB - LeetPush

---
+++ 1848-minimum-distance-to-the-target-element/solution.py
@@ -0,0 +1,17 @@
+# Minimum Distance to the Target Element
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.5 MB
+# https://leetcode.com/problems/minimum-distance-to-the-target-element/
+
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        min_dist = float('inf')
+        
+        for i in range(len(nums)):
+        :type nums: List[int]
+        """
+    def getMinDistance(self, nums, target, start):
+class Solution(object):
