e5ede1961b06b4b99a5136bacd4e7dd6c1084855
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-15 17:09:00 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ 33-search-in-rotated-sorted-array/solution.py
@@ -0,0 +1,18 @@
+# Search in Rotated Sorted Array
+# Difficulty: Medium
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+            mid = (left + right) // 2
+        
+        while left <= right:
+        left, right = 0, len(nums) - 1
+
+        """
+        :rtype: int
+        :type nums: List[int]
+        :type target: int
+        """
+    def search(self, nums, target):
+            
