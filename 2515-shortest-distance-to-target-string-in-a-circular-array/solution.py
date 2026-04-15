3ca8b22be2b53e6122cf241da98c3da658a24b07
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-15 17:06:26 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/solution.py
@@ -0,0 +1,17 @@
+# Shortest Distance to Target String in a Circular Array
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/
+
+        
+        ans = float('inf')
+        n = len(words)
+        """
+        :rtype: int
+        :type target: str
+        :type startIndex: int
+        :type words: List[str]
+        """
+    def closestTarget(self, words, target, startIndex):
+class Solution(object):
