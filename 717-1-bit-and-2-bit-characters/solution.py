859b48b559b31775c13f8eda69e8d136cc0e58fc
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-16 16:45:08 +0530

    Time: 0 ms | Memory: 19.1 MB - LeetPush

---
+++ 717-1-bit-and-2-bit-characters/solution.py
@@ -0,0 +1,17 @@
+# 1-bit and 2-bit Characters
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 19.1 MB
+# https://leetcode.com/problems/1-bit-and-2-bit-characters/
+
+        i = 0
+        n = len(bits)
+        
+        while i < n - 1:
+            if bits[i] == 1:
+                i += 2
+            else:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+class Solution:
+                i += 1
+                
