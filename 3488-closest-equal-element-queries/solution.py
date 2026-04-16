adf999d582783c033502c6042bba397c7d6c709a
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-16 15:26:34 +0530

    Time: 312 ms | Memory: 60.1 MB - LeetPush

---
+++ 3488-closest-equal-element-queries/solution.py
@@ -0,0 +1,17 @@
+# Closest Equal Element Queries
+# Difficulty: Medium
+# Runtime: 312 ms
+# Memory: 60.1 MB
+# https://leetcode.com/problems/closest-equal-element-queries/
+
+            x = pos[0]
+            pos.insert(0, pos[-1] - n)
+        for pos in nums_pos.values():
+
+            nums_pos[nums[i]].append(i)
+        nums_pos = defaultdict(list)
+
+        for i in range(n):
+    def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        n = len(nums)
+class Solution:
