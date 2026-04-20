5abd3e947ac28e59f0b7ddc8ae9acb8ff96fddda
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-20 16:04:57 +0530

    Time: 966 ms | Memory: 14 MB - LeetPush

---
+++ 212-word-search-ii/solution.py
@@ -0,0 +1,13 @@
+# Word Search II
+# Difficulty: Hard
+# Runtime: 966 ms
+# Memory: 14 MB
+# https://leetcode.com/problems/word-search-ii/
+
+                node.pop(char)
+
+        for i in range(n):
+            for j in range(m):
+                dfs(i, j, trie)
+
+        return result
