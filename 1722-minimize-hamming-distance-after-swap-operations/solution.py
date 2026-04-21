ee270681b9ae2033b7fc6a14c7f0507e6c1764a5
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-21 15:32:07 +0530

    Time: 398 ms | Memory: 48.2 MB - LeetPush

---
+++ 1722-minimize-hamming-distance-after-swap-operations/solution.py
@@ -0,0 +1,17 @@
+# Minimize Hamming Distance After Swap Operations
+# Difficulty: Medium
+# Runtime: 398 ms
+# Memory: 48.2 MB
+# https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/
+
+
+        """
+        :rtype: int
+        :type allowedSwaps: List[List[int]]
+        :type target: List[int]
+        :type source: List[int]
+        """
+from collections import defaultdict, Counter
+    def minimumHammingDistance(self, source, target, allowedSwaps):
+class Solution(object):
+
