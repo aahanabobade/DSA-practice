d0b8f9a24dd64deb0d1c367493da021a358ce5ed
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-25 09:35:37 +0530

    Time: 2168 ms | Memory: 18.4 MB - LeetPush

---
+++ 3464-maximize-the-distance-between-points-on-a-square/solution.py
@@ -0,0 +1,18 @@
+# Maximize the Distance Between Points on a Square
+# Difficulty: Hard
+# Runtime: 2168 ms
+# Memory: 18.4 MB
+# https://leetcode.com/problems/maximize-the-distance-between-points-on-a-square/
+
+            for i in range(INF):
+            j = 1
+            nxt = [INF] * INF
+        def can(d):
+
+        INF = 2 * n
+        LOG = max(1, k.bit_length())
+
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
+        def manhattan(a, b):
+        pts = points + points  
+
