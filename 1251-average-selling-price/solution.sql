b8da6386af4b66e8fad9c5fe64bff8947d9e68d1
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-25 11:09:55 +0530

    Time: 797 ms | Memory: 0B - LeetPush

--- 1251-average-selling-price/solution.sql
+++ 1251-average-selling-price/solution.sql
@@ -1,17 +1,15 @@
 -- Average Selling Price
 -- Difficulty: Easy
--- Runtime: 831 ms
+-- Runtime: 797 ms
 -- Memory: 0B
 -- https://leetcode.com/problems/average-selling-price/
 
+# Write your MySQL query statement below
 SELECT 
     p.product_id,
-    ROUND(
-        COALESCE(SUM(u.units * p.price) / SUM(u.units), 0),
-        2
-    ) AS average_price
+    IFNULL(ROUND(SUM(u.units * p.price) / SUM(u.units), 2), 0) AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
     AND u.purchase_date BETWEEN p.start_date AND p.end_date
-# Write your MySQL query statement below
+GROUP BY p.product_id;
