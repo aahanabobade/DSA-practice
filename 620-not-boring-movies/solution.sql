b0f4c45d9069974b1af0778099d5c456b56fc9d1
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-25 10:24:24 +0530

    Time: 258 ms | Memory: 0B - LeetPush

--- 620-not-boring-movies/solution.sql
+++ 620-not-boring-movies/solution.sql
@@ -1,12 +1,10 @@
 -- Not Boring Movies
 -- Difficulty: Easy
--- Runtime: 246 ms
+-- Runtime: 258 ms
 -- Memory: 0B
 -- https://leetcode.com/problems/not-boring-movies/
 
 # Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE id % 2 = 1
-  AND description != 'boring'
-ORDER BY rating DESC;
+select * from Cinema
+where id%2!=0 and description != 'boring'
+order by rating desc
