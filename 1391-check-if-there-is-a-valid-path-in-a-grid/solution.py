16acec9b04638ea25a427581e85ed7f1d6066474
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-27 16:37:20 +0530

    Time: 203 ms | Memory: 14.3 MB - LeetPush

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/solution.py
@@ -0,0 +1,17 @@
+# Check if There is a Valid Path in a Grid
+# Difficulty: Medium
+# Runtime: 203 ms
+# Memory: 14.3 MB
+# https://leetcode.com/problems/check-if-there-is-a-valid-path-in-a-grid/
+
+        m, n = len(grid), len(grid[0])
+
+        """
+        :rtype: bool
+        :type grid: List[List[int]]
+        """
+    def hasValidPath(self, grid):
+from collections import deque
+class Solution(object):
+
+
