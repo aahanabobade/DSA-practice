11d18c92947a406032906ba143f5562b5d9fb659
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-04-29 11:00:29 +0530

    Time: 1403 ms | Memory: 0B - LeetPush

--- 1378-replace-employee-id-with-the-unique-identifier/solution.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,12 +1,11 @@
 -- Replace Employee ID With The Unique Identifier
 -- Difficulty: Easy
--- Runtime: 1181 ms
+-- Runtime: 1403 ms
 -- Memory: 0B
 -- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
 
-SELECT 
-    eu.unique_id,
-    e.name
-FROM Employees e
-LEFT JOIN EmployeeUNI eu
-ON e.id = eu.id;
+
+select eu.unique_id , e.name
+from Employees e
+left join EmployeeUNI eu
+on e.id = eu.id
