c91dc467afdb66c08a76f9c9818618cf15358e0d
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-06 15:50:08 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ concatenation-of-array/solution.py
@@ -0,0 +1,16 @@
+# Q1. Concatenation of Array
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/concatenation-of-array/
+
+        ans =[]
+
+        for i in range(n):
+            ans.append(nums[i])
+        
+        for i in range(n):
+            ans.append(nums[i])
+        
+        return ans
+        
