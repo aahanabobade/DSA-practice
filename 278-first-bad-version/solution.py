c2950862f529075c9733e60073d0e08d6cb0c5c5
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-14 19:37:28 +0530

    Time: 15 ms | Memory: 12.4 MB - LeetPush

--- 278-first-bad-version/solution.py
+++ 278-first-bad-version/solution.py
@@ -1,15 +1,30 @@
 # First Bad Version
 # Difficulty: Easy
 # Runtime: 15 ms
-# Memory: 12.3 MB
+# Memory: 12.4 MB
 # https://leetcode.com/problems/first-bad-version/
 
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+    
+#use 2 pointer
+        left =1
+        right =n
 
-·‌·‌·‌·‌·‌·‌·‌·‌while·‌left<right:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌mid·‌=·‌(left+right)//2
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌if·‌isBadVersion(mid):
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌else:
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌right·‌=·‌mid
-·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌·‌left·‌=·‌mid+1
-·‌·‌·‌·‌·‌·‌·‌·‌
-·‌·‌·‌·‌·‌·‌·‌·‌return·‌left
+        while left<right:
+            mid = left+(right-left)//2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left =mid+1
+        
+        return left
