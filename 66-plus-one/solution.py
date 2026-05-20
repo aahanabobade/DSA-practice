608c4bbd28e9296c18792d3254c628ac301b17fb
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-20 17:05:38 +0530

    Time: 0 ms | Memory: 12.3 MB - LeetPush

--- 66-plus-one/solution.py
+++ 66-plus-one/solution.py
@@ -4,13 +4,14 @@
 # Memory: 12.3 MB
 # https://leetcode.com/problems/plus-one/
 
-
-        for i in range(n-1,-1,-1):
-            if digits[i]<9:
-        n = len(digits)
-        """
         :rtype: List[int]
-        :type digits: List[int]
         """
-    def plusOne(self, digits):
-class Solution(object):
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            
+            digits[i]=0
+        
+        return [1]+digits
