126bd4353b0a69ea3ab4b30ebf1a7ed7efda5274
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-20 11:27:37 +0530

    Time: 0 ms | Memory: 13.2 MB - LeetPush

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -4,21 +4,14 @@
 # Memory: 13.2 MB
 # https://leetcode.com/problems/two-sum/
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
         :rtype: List[int]
         """
         seen = {}
 
         n = len(nums)
-
         for i in range(n):
-            if comp in seen:
-                
             comp = target -nums[i]
+            if comp in seen:
                 return [seen[comp],i]
-            
+        
             seen[nums[i]]=i
