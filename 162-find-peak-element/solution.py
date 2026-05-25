68a33e63c41f3af01a49f92062cb473eb9b3bb9b
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-25 12:08:32 +0530

    Time: 0 ms | Memory: 12.3 MB - LeetPush

--- 162-find-peak-element/solution.py
+++ 162-find-peak-element/solution.py
@@ -1,25 +1,21 @@
 # Find Peak Element
 # Difficulty: Medium
 # Runtime: 0 ms
-# Memory: 12.4 MB
+# Memory: 12.3 MB
 # https://leetcode.com/problems/find-peak-element/
 
-class Solution(object):
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         n = len(nums)
         left = 0
         right = n-1
 
         while left<right:
-            mid = left+(right-left)//2
+            mid = (right+left)//2
 
             if nums[mid]<nums[mid+1]:
                 left = mid+1
+            
             else:
                 right = mid
         
         return left
+
