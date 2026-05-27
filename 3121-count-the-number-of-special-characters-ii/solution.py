e9832d12a806bd6e45e48a24e2a2f620518ae01b
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-27 10:21:13 +0530

    Time: 312 ms | Memory: 14.9 MB - LeetPush

---
+++ 3121-count-the-number-of-special-characters-ii/solution.py
@@ -0,0 +1,22 @@
+# Count the Number of Special Characters II
+# Difficulty: Medium
+# Runtime: 312 ms
+# Memory: 14.9 MB
+# https://leetcode.com/problems/count-the-number-of-special-characters-ii/
+
+                if lower not in firstUpper:
+
+
+            else:
+                lower = ch.lower()
+            if ch.islower():
+                lastLower[ch] = i
+
+        for i, ch in enumerate(word):
+
+        firstUpper = {}
+        lastLower = {}
+
+        """
+        :rtype: int
+        :type word: str
