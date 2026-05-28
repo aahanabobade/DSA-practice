e682aa10a725d97bb082c5ff7f9b680001c4c0da
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-28 13:25:03 +0530

    Time: 4586 ms | Memory: 340.9 MB - LeetPush

---
+++ 3093-longest-common-suffix-queries/solution.py
@@ -0,0 +1,21 @@
+# Longest Common Suffix Queries
+# Difficulty: Hard
+# Runtime: 4586 ms
+# Memory: 340.9 MB
+# https://leetcode.com/problems/longest-common-suffix-queries/
+
+               len(word) < len(wordsContainer[root.best_idx]) or \
+            if root.best_idx == -1 or \
+            # Update root with best candidate (shortest len, then earliest idx)
+            node = root
+        for i, word in enumerate(wordsContainer):
+        
+        root = TrieNode()
+        # Build the trie on reversed container words
+    def stringIndices(self, wordsContainer, wordsQuery):
+class Solution:
+
+        self.best_idx = -1  # stores best candidate at this node
+        self.children = {}
+    def __init__(self):
+class TrieNode:
