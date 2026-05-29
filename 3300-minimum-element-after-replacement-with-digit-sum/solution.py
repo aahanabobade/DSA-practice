3849121ec250789f5647aad500339df22dbc98fb
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-05-29 09:24:09 +0530

    Time: 8 ms | Memory: 12.4 MB - LeetPush

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/solution.py
@@ -0,0 +1,21 @@
+# Minimum Element After Replacement With Digit Sum
+# Difficulty: Easy
+# Runtime: 8 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/minimum-element-after-replacement-with-digit-sum/
+
+            summ = 0
+            num = nums[i] #assign element in array as num
+            while num > 0:
+        for i in range(n):
+        
+
+        n = len(nums) # len
+
+        mini = float('inf') #min
+        :rtype: int
+        """
+        :type nums: List[int]
+        """
+    def minElement(self, nums):
+class Solution(object):
