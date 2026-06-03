7a9be3bb80b18782cc82c9cf6277833580b75d0c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-03 11:59:36 +0530

    Time: 0 ms | Memory: 12.3 MB - LeetPush

--- 88-merge-sorted-array/solution.py
+++ 88-merge-sorted-array/solution.py
@@ -4,9 +4,17 @@
 # Memory: 12.3 MB
 # https://leetcode.com/problems/merge-sorted-array/
 
-            k-=1
-        
-        while j >=0:
-            nums1[k]=nums2[j]
-            j-=1
-            k-=1
+        p=m+n-1
+        while p1>= 0 and p2>=0:
+            if nums1[p1]>nums2[p2]:
+                nums1[p]=nums1[p1]
+                p1-=1
+            else:
+                nums1[p]=nums2[p2]
+                p2-=1
+            p-=1
+
+        while p2>=0:
+            nums1[p]=nums2[p2]
+            p2-=1
+            p-=1
