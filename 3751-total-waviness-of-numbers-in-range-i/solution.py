288fdaaf1bc42c2b0205bf9629848f3f1cc70de9
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-04 08:48:13 +0530

    Time: 1291 ms | Memory: 15.1 MB - LeetPush

---
+++ 3751-total-waviness-of-numbers-in-range-i/solution.py
@@ -0,0 +1,21 @@
+# Total Waviness of Numbers in Range I
+# Difficulty: Medium
+# Runtime: 1291 ms
+# Memory: 15.1 MB
+# https://leetcode.com/problems/total-waviness-of-numbers-in-range-i/
+
+                continue
+            if len(s) < 3:
+            # only numbers with 3+ digits can have peaks/valleys
+            
+            s = str(num)
+        for num in range(num1, num2 + 1):
+
+        total = 0
+        """
+        :rtype: int
+        :type num2: int
+        :type num1: int
+        """
+    def totalWaviness(self, num1, num2):
+class Solution(object):
