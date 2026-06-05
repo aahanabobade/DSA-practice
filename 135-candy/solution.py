cfc3a308b955e5cebdac385e5bbddec1cb16ba9e
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:21:10 +0530

    Time: 22 ms | Memory: 14.7 MB - LeetPush

---
+++ 135-candy/solution.py
@@ -0,0 +1,25 @@
+# Candy
+# Difficulty: Hard
+# Runtime: 22 ms
+# Memory: 14.7 MB
+# https://leetcode.com/problems/candy/
+
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1]*n
+
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                candies[i]=candies[i-1]+1
+        
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                candies[i]=max(candies[i],candies[i+1]+1)
+        
+        return sum (candies)
+        
