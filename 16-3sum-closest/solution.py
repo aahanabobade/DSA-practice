fdb293fba6bf0f85b5be48d6efca093ecf2fbd32
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:23:23 +0530

    Time: 478 ms | Memory: 12.4 MB - LeetPush

---
+++ 16-3sum-closest/solution.py
@@ -0,0 +1,33 @@
+# 3Sum Closest
+# Difficulty: Medium
+# Runtime: 478 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/3sum-closest/
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        n = len(nums)
+        closest_sum = float('inf')
+
+
+        for i in range(n):
+            if i >0 and nums[i]==nums[i-1]:
+                continue
+            
+            lo,hi =i+1,n-1
+            while lo <hi:
+                cur_sum =nums[i]+nums[lo]+nums[hi]
+
+                if abs(cur_sum -target)< abs(closest_sum - target):
+                    closest_sum = cur_sum
+
+                if cur_sum == target:
+                    return cur_sum
+                elif cur_sum<target:
+                    lo+=1
+                else:
+                    hi-=1
+            
+        return closest_sum
+                
