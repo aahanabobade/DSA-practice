5d0287c44eb0cbeeaaabc5941c5c94f71b5286de
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:24:35 +0530

    Time: 2284 ms | Memory: 0B - LeetPush

---
+++ 577-employee-bonus/solution.sql
@@ -0,0 +1,11 @@
+-- Employee Bonus
+-- Difficulty: Easy
+-- Runtime: 2284 ms
+-- Memory: 0B
+-- https://leetcode.com/problems/employee-bonus/
+
+select e.name, b.bonus
+from Employee e
+left join Bonus b
+on b.empId = e.empId
+where b.bonus < 1000 OR b.bonus IS NULL;
