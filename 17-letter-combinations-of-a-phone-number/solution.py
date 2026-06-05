be2df445361e504bd4fb1beffb3417c081b9ab89
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:23:19 +0530

    Time: 0 ms | Memory: 12.3 MB - LeetPush

---
+++ 17-letter-combinations-of-a-phone-number/solution.py
@@ -0,0 +1,30 @@
+# Letter Combinations of a Phone Number
+# Difficulty: Medium
+# Runtime: 0 ms
+# Memory: 12.3 MB
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        if digits ==" ":
+            return []
+
+        ans, sol =[],[]
+
+        letter_map={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+
+        n = len(digits) 
+
+        def backtrack(i=0):
+            if i ==n:
+                ans.append(''.join(sol))
+                return
+
+            for letter in letter_map[digits[i]]:
+                sol.append(letter)
+                backtrack(i+1)
+                sol.pop()
+
+        backtrack(0)
+        return ans
+        
