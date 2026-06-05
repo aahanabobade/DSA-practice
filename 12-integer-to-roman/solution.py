35e3983f71539f9d32b014331d641093351a970e
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:23:31 +0530

    Time: 7 ms | Memory: 12.6 MB - LeetPush

---
+++ 12-integer-to-roman/solution.py
@@ -0,0 +1,21 @@
+# Integer to Roman
+# Difficulty: Medium
+# Runtime: 7 ms
+# Memory: 12.6 MB
+# https://leetcode.com/problems/integer-to-roman/
+
+class Solution(object):
+    def intToRoman(self, num):
+        symList = [["I",1],["IV",4],["V",5],["IX",9],["X",10],["XL",40],["L",50],  ["XC",90],["C",100],["CD",400],["D",500],["CM",900],["M",1000]]
+
+
+        res =""
+        for sym,val in reversed(symList):
+            if num//val:
+                count = num//val
+                res+=(sym*count)
+                num = num%val
+        
+        return res
+        
+        
