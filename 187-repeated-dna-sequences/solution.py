4942a0ea9cae293d3be5bb8c9bb77209b8bdc5d9
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:21:48 +0530

    Time: 32 ms | Memory: 28.8 MB - LeetPush

---
+++ 187-repeated-dna-sequences/solution.py
@@ -0,0 +1,25 @@
+# Repeated DNA Sequences
+# Difficulty: Medium
+# Runtime: 32 ms
+# Memory: 28.8 MB
+# https://leetcode.com/problems/repeated-dna-sequences/
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
+        seen = set()
+        repeated = set()
+        
+        for i in range(len(s) - 9):
+            seq = s[i:i+10]
+            
+            if seq in seen:
+                repeated.add(seq)
+            else:
+                seen.add(seq)
+        
+        return list(repeated)
