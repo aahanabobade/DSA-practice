d58fb043059aae85f0d7f8b33fd1d0f8a4e66bef
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-05 12:22:00 +0530

    Time: 0 ms | Memory: 12.6 MB - LeetPush

---
+++ 733-flood-fill/solution.py
@@ -0,0 +1,32 @@
+# Flood Fill
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.6 MB
+# https://leetcode.com/problems/flood-fill/
+
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        rows = len(image)
+        cols = len(image[0])
+        
+        original = image[sr][sc]
+        
+        if original == color:
+            return image
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return
+            
+            if image[r][c] != original:
+                return
+            
+            image[r][c] = color
+            
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        dfs(sr, sc)
+        return image
