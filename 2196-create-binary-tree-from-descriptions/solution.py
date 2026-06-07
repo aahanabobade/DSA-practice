db586a528bd70875489249ea9ae6dc69a78baee8
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-07 10:23:26 +0530

    Time: 313 ms | Memory: 24.2 MB - LeetPush

---
+++ 2196-create-binary-tree-from-descriptions/solution.py
@@ -0,0 +1,22 @@
+# Create Binary Tree From Descriptions
+# Difficulty: Medium
+# Runtime: 313 ms
+# Memory: 24.2 MB
+# https://leetcode.com/problems/create-binary-tree-from-descriptions/
+
+        children = set()
+        node = {}
+        :rtype: Optional[TreeNode]
+        """
+        :type descriptions: List[List[int]]
+        """
+class Solution(object):
+    def createBinaryTree(self, descriptions):
+
+from collections import defaultdict
+#         self.right = right
+#         self.left = left
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+# class TreeNode(object):
+# Definition for a binary tree node.
