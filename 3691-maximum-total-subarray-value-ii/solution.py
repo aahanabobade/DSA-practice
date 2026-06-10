18ec65452ed0e579166a80201220c21749592b3f
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-10 12:49:32 +0530

    Time: 4254 ms | Memory: 36.9 MB - LeetPush

---
+++ 3691-maximum-total-subarray-value-ii/solution.py
@@ -0,0 +1,21 @@
+# Maximum Total Subarray Value II
+# Difficulty: Hard
+# Runtime: 4254 ms
+# Memory: 36.9 MB
+# https://leetcode.com/problems/maximum-total-subarray-value-ii/
+
+        st_min = [nums[:]]
+        st_max = [nums[:]]
+        m = lg[n] + 1
+
+
+            lg[i] = lg[i // 2] + 1
+        for i in range(2, n + 1):
+        lg = [0] * (n + 1)
+        # logs
+
+        n = len(nums)
+    def maxTotalValue(self, nums, k):
+class Solution:
+
+import heapq
