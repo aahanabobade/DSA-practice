e772a74634b29ebd8913728faf3308dbe72207b8
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-17 15:28:29 +0530

    Time: 816 ms | Memory: 19.7 MB - LeetPush

---
+++ 3614-process-string-with-special-operations-ii/solution.py
@@ -0,0 +1,21 @@
+# Process String with Special Operations II
+# Difficulty: Hard
+# Runtime: 816 ms
+# Memory: 19.7 MB
+# https://leetcode.com/problems/process-string-with-special-operations-ii/
+
+            if 'a' <= ch <= 'z':
+
+            cur = length[i]
+        # Forward pass: compute lengths
+        for i, ch in enumerate(s):
+
+        length = [0] * (n + 1)
+        n = len(s)
+        """
+        :rtype: str
+        :type k: int
+        :type s: str
+        """
+    def processStr(self, s, k):
+class Solution(object):
