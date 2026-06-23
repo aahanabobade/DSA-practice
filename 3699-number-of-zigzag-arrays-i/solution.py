fae11da1416e24a2d796e2163a510fe5afd64a98
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-23 11:17:16 +0530

    Time: 11228 ms | Memory: 12.9 MB - LeetPush

---
+++ 3699-number-of-zigzag-arrays-i/solution.py
@@ -0,0 +1,21 @@
+# Number of ZigZag Arrays I
+# Difficulty: Hard
+# Runtime: 11228 ms
+# Memory: 12.9 MB
+# https://leetcode.com/problems/number-of-zigzag-arrays-i/
+
+
+        dp0 = [1] * m
+        dp1 = [1] * m
+        MOD = 1000000007
+        m = r - l + 1
+    def zigZagArrays(self, n, l, r):
+            res.append(s)
+        return res
+
+        for x in arr:
+            s += x
+        s = initial
+        res = [initial]
+    def accumulate_with_initial(self, arr, initial=0):
+class Solution(object):
