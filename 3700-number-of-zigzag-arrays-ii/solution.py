a0c87b872bf9695ffa1f4154169ebeb1b959f7f6
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-06-24 09:59:29 +0530

    Time: 1879 ms | Memory: 12.7 MB - LeetPush

---
+++ 3700-number-of-zigzag-arrays-ii/solution.py
@@ -0,0 +1,21 @@
+# Number of ZigZag Arrays II
+# Difficulty: Hard
+# Runtime: 1879 ms
+# Memory: 12.7 MB
+# https://leetcode.com/problems/number-of-zigzag-arrays-ii/
+
+                    if Xi[k] == 0:
+                for k in range(m):
+                Xi = X[i]
+                Zi = Z[i]
+            for i in range(m):
+
+        def mat_mul(X, Y):
+            Z = [[0] * m for _ in range(m)]
+        # A[i][j] = min(i, j) in 0-based indexing
+        A = [[min(i, j) % MOD for j in range(m)] for i in range(m)]
+
+        MOD = 10**9 + 7
+        m = r - l + 1
+    def zigZagArrays(self, n, l, r):
+class Solution(object):
