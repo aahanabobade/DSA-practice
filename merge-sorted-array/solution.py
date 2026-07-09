03ac743139cceca5e5b8258911ec99d2188ad794
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-09 22:20:04 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ merge-sorted-array/solution.py
@@ -0,0 +1,19 @@
+# Merge Sorted Array
+# Difficulty: Unknown
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/merge-sorted-array/
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i = m-1
+        j= n-1
+        p = m+n-1
+
