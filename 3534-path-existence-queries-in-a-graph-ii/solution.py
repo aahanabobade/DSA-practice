e650c04ad2cb50126b705004ef46fcce5ef0c0bc
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-10 23:04:09 +0530

    Time: 1673 ms | Memory: 70.2 MB - LeetPush

---
+++ 3534-path-existence-queries-in-a-graph-ii/solution.py
@@ -0,0 +1,13 @@
+# Path Existence Queries in a Graph II
+# Difficulty: Hard
+# Runtime: 1673 ms
+# Memory: 70.2 MB
+# https://leetcode.com/problems/path-existence-queries-in-a-graph-ii/
+
+        :type n: int
+        :type nums: List[int]
+        :type maxDiff: int
+        :type queries: List[List[int]]
+class Solution(object):
+    def pathExistenceQueries(self, n, nums, maxDiff, queries):
+        """
