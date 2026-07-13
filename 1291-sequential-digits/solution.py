66c58b50873b11c2de6fa655034c2087b79a85ee
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-13 20:12:25 +0530

    Time: 0 ms | Memory: 12.5 MB - LeetPush

---
+++ 1291-sequential-digits/solution.py
@@ -0,0 +1,15 @@
+# Sequential Digits
+# Difficulty: Medium
+# Runtime: 0 ms
+# Memory: 12.5 MB
+# https://leetcode.com/problems/sequential-digits/
+
+                num = int(s[start:start + length])
+                if low <= num <= high:
+            for start in range(10 - length):
+        for length in range(low_len, high_len + 1):
+        high_len = len(str(high))
+
+        low_len = len(str(low))
+
+        ans = []
