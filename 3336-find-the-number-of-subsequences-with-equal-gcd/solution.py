997b1229282dfb3dce992be14df1dfa7d3f73a78
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-14 22:06:11 +0530

    Time: 2138 ms | Memory: 13.6 MB - LeetPush

---
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/solution.py
@@ -0,0 +1,14 @@
+# Find the Number of Subsequences With Equal GCD
+# Difficulty: Hard
+# Runtime: 2138 ms
+# Memory: 13.6 MB
+# https://leetcode.com/problems/find-the-number-of-subsequences-with-equal-gcd/
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+
+class Solution(object):
+    def subsequencePairCount(self, nums):
