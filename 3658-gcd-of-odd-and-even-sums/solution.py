d536beab0ecdee01c1e1f807c63053756b2ad808
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-15 08:29:17 +0530

    Time: 0 ms | Memory: 12.5 MB - LeetPush

---
+++ 3658-gcd-of-odd-and-even-sums/solution.py
@@ -0,0 +1,13 @@
+# GCD of Odd and Even Sums
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.5 MB
+# https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n
