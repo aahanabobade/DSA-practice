c437eee128757b09b37b69241ef7665bacaec7f6
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-22 15:17:34 +0530

    Time: 1838 ms | Memory: 63.3 MB - LeetPush

---
+++ 3501-maximize-active-section-with-trade-ii/solution.py
@@ -0,0 +1,11 @@
+# Maximize Active Section with Trade II
+# Difficulty: Hard
+# Runtime: 1838 ms
+# Memory: 63.3 MB
+# https://leetcode.com/problems/maximize-active-section-with-trade-ii/
+
+        """
+    def maxActiveSectionsAfterTrade(self, s, queries):
+class Solution(object):
+
+import bisect
