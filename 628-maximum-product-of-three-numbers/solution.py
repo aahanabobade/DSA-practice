5c4153700c8ca446347b24b5cdad2fa447b83682
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-26 12:14:11 +0530

    Time: 26 ms | Memory: 13.3 MB - LeetPush

---
+++ 628-maximum-product-of-three-numbers/solution.py
@@ -0,0 +1,14 @@
+# Maximum Product of Three Numbers
+# Difficulty: Easy
+# Runtime: 26 ms
+# Memory: 13.3 MB
+# https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+        nums.sort()
+        return max(
+        """
+        :rtype: int
+        :type nums: List[int]
+        """
+    def maximumProduct(self, nums):
+class Solution(object):
