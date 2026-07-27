31af0cc51f6939972ed18f17ee0ed1b4cf94358c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-27 20:15:33 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

---
+++ 1464-maximum-product-of-two-elements-in-an-array/solution.py
@@ -0,0 +1,11 @@
+# Maximum Product of Two Elements in an Array
+# Difficulty: Easy
+# Runtime: 0 ms
+# Memory: 12.4 MB
+# https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+
+                first = num
+            elif num > second:
+                second = num
+
+        return (first - 1) * (second - 1)
