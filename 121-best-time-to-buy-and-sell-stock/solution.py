5f4ecb4fb838567509d5ed094b8c9b8060615c57
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 11:59:12 +0530

    Time: 50 ms | Memory: 19.5 MB - LeetPush

--- 121-best-time-to-buy-and-sell-stock/solution.py
+++ 121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,19 +1,7 @@
 # Best Time to Buy and Sell Stock
 # Difficulty: Easy
-# Runtime: 35 ms
-# Memory: 18.9 MB
+# Runtime: 50 ms
+# Memory: 19.5 MB
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-
-            if profit> max_profit:
-                max_profit = profit
-
-        return max_profit
-
-
-            profit = i - min_price
-            
-                min_price = i
-            if i <min_price:
-        for i in prices:
-
+ 
