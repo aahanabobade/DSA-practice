18861908a96409c3fda89486c5662ada9e8cb20c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 18:27:59 +0530

    Time: 105 ms | Memory: 21.3 MB - LeetPush

--- 53-maximum-subarray/solution.py
+++ 53-maximum-subarray/solution.py
@@ -1,21 +1,21 @@
 # Maximum Subarray
 # Difficulty: Medium
-# Runtime: 106 ms
-# Memory: 21.1 MB
+# Runtime: 105 ms
+# Memory: 21.3 MB
 # https://leetcode.com/problems/maximum-subarray/
 
+class Solution(object):
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
         n = len(nums)
+        cur = nums[0]
         maxx = nums[0]
+
         for i in range(1,n):
-        curr = nums[0]
+            cur= max(nums[i],cur+nums[i])
+            maxx = max(cur, maxx)
 
-            curr = max(nums[i],curr+nums[i])
-            maxx = max(maxx, curr)
-
-        return maxx    
-        
+        return maxx
