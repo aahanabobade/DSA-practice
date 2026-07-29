1b3cda630ef73e8885067652ee7375dfd9c93860
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 11:27:39 +0530

    Time: 3 ms | Memory: 14 MB - LeetPush

--- 26-remove-duplicates-from-sorted-array/solution.py
+++ 26-remove-duplicates-from-sorted-array/solution.py
@@ -1,18 +1,19 @@
 # Remove Duplicates from Sorted Array
 # Difficulty: Easy
-# Runtime: 4 ms
-# Memory: 13.9 MB
+# Runtime: 3 ms
+# Memory: 14 MB
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-        k = 1
-
-        for i in range(1,n):
             if nums[i]!=nums[i-1]:
-                nums[k]= nums[i]
+                nums[k]=nums[i]
                 k+=1
         
         return k
+        for i in range(1,n):
+
+        k = 1
+        n = len(nums)
+        """
+        :rtype: int
+            
+
