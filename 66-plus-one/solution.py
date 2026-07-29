96890044d20882d2d203bd3ea35a2ad3319e101c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 12:20:47 +0530

    Time: 0 ms | Memory: 12.4 MB - LeetPush

--- 66-plus-one/solution.py
+++ 66-plus-one/solution.py
@@ -1,17 +1,8 @@
 # Plus One
 # Difficulty: Easy
 # Runtime: 0 ms
-# Memory: 12.3 MB
+# Memory: 12.4 MB
 # https://leetcode.com/problems/plus-one/
 
-        :rtype: List[int]
-        """
-        n = len(digits)
-        for i in range(n - 1, -1, -1):
-            if digits[i]<9:
-                digits[i]+=1
-                return digits
-            
-            digits[i]=0
-        
-        return [1]+digits
+ 
+ 
