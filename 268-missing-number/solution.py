68aafa36a7511a7d741ea297f8e9f5d06b463655
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 12:24:00 +0530

    Time: 0 ms | Memory: 13.3 MB - LeetPush

--- 268-missing-number/solution.py
+++ 268-missing-number/solution.py
@@ -1,18 +1,7 @@
 # Missing Number
 # Difficulty: Easy
 # Runtime: 0 ms
-# Memory: 13.4 MB
+# Memory: 13.3 MB
 # https://leetcode.com/problems/missing-number/
 
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        n = len(nums)
-
-        expected = n * (n + 1) // 2
-        actual = sum(nums)
-
-        return expected - actual
-
+c
