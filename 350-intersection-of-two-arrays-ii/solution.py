456c09cbc33642cee26906abefeaeb14b62d14e9
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 19:33:59 +0530

    Time: 2 ms | Memory: 12.4 MB - LeetPush

--- 350-intersection-of-two-arrays-ii/solution.py
+++ 350-intersection-of-two-arrays-ii/solution.py
@@ -1,21 +1,7 @@
 # Intersection of Two Arrays II
 # Difficulty: Easy
-# Runtime: 0 ms
-# Memory: 12.5 MB
+# Runtime: 2 ms
+# Memory: 12.4 MB
 # https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
-
-        for i in nums1:
-            freq[i] = freq.get(i, 0) + 1
-
-        # check nums2
-        for i in nums2:
-        result = []
-            if i in freq and freq[i] > 0:
-                result.append(i)
-                freq[i] -= 1
-
-        return result
-        freq = {}
-        """
-        :rtype: List[int]
+ 
