f9f3bc28243c2da63757dc5ed64cee23e831afd6
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-29 11:18:18 +0530

    Time: 0 ms | Memory: 13.2 MB - LeetPush

--- 1-two-sum/solution.py
+++ 1-two-sum/solution.py
@@ -4,14 +4,5 @@
 # Memory: 13.2 MB
 # https://leetcode.com/problems/two-sum/
 
-        :rtype: List[int]
-        """
-        seen = {}
-
-        n = len(nums)
-        for i in range(n):
-            comp = target -nums[i]
-            if comp in seen:
-                return [seen[comp],i]
-        
-            seen[nums[i]]=i
+ 
+ 
