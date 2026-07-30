5e399e8672603782956a5bc106efd55b6c16751c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-30 20:29:43 +0530

    Time: 823 ms | Memory: 18.8 MB - LeetPush

--- 15-3sum/solution.py
+++ 15-3sum/solution.py
@@ -1,18 +1,28 @@
 # 3Sum
 # Difficulty: Medium
-# Runtime: 768 ms
-# Memory: 18.3 MB
+# Runtime: 823 ms
+# Memory: 18.8 MB
 # https://leetcode.com/problems/3sum/
 
-                total = nums[i] + nums[left] + nums[right]
 
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
+                if total ==0:
+                    ans.append([nums[i],nums[left],nums[right]])
 
-                    # skip duplicates for left
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
+                    left+=1
+                    right-=1
 
-                    # skip duplicates for right
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left+=1
+                total = nums[i]+nums[left]+nums[right]
+            while left<right:
+
+                    while left < right and nums[right] == nums[right+1]:
+                        right-=1
+                
+                elif total <0:
+                    left+=1
+                
+                elif total>0:
+                    right-=1
+        
+        return ans
