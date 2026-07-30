ce012c0b3f8835df98188ee7460e5efd1f727ae8
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-30 12:53:38 +0530

    Time: 0 ms | Memory: 12.9 MB - LeetPush

--- 414-third-maximum-number/solution.py
+++ 414-third-maximum-number/solution.py
@@ -1,22 +1,7 @@
 # Third Maximum Number
 # Difficulty: Easy
 # Runtime: 0 ms
-# Memory: 12.8 MB
+# Memory: 12.9 MB
 # https://leetcode.com/problems/third-maximum-number/
 
-            
-            elif i > sec:
-                third = sec
-                sec = i
-            
-            elif i >third:
-                third = i
-        
-        if third == float('-inf'):
-            return first
-                first = i
-                sec = first
-                third = sec
-            if i > first:
-            
-                continue
+ 
