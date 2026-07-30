790ef0ce2667bc8e3e382c27819a562b8c9de295
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-07-30 22:37:00 +0530

    Time: 121 ms | Memory: 20.8 MB - LeetPush

--- 11-container-with-most-water/solution.py
+++ 11-container-with-most-water/solution.py
@@ -1,17 +1,7 @@
 # Container With Most Water
 # Difficulty: Medium
-# Runtime: 123 ms
-# Memory: 20.6 MB
+# Runtime: 121 ms
+# Memory: 20.8 MB
 # https://leetcode.com/problems/container-with-most-water/
 
-
-            h = min(height[left], height[right])
-            w = right - left
-            area = h * w
-
-        while left < right:
-        max_area = 0
-class Solution(object):
-    def maxArea(self, height):
-        left = 0
-        right = len(height) - 1
+ 
