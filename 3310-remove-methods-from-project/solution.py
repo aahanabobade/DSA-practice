87f476a1337ccafb1de9a84d1eef03c3cf71cc3c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-08-05 10:51:59 +0530

    Time: 465 ms | Memory: 91.2 MB - LeetPush

---
+++ 3310-remove-methods-from-project/solution.py
@@ -0,0 +1,19 @@
+# Remove Methods From Project
+# Difficulty: Medium
+# Runtime: 465 ms
+# Memory: 91.2 MB
+# https://leetcode.com/problems/remove-methods-from-project/
+
+        suspicious = [False] * n
+        # Find all suspicious methods
+            graph[u].append(v)
+
+        for u, v in invocations:
+
+        graph = defaultdict(list)
+        """
+        :rtype: List[int]
+        :type invocations: List[List[int]]
+        :type k: int
+        :type n: int
+        """
