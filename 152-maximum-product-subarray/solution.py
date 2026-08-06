1a03be81fc157ec4908d98a44e4b386c59f9d33d
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-08-06 19:56:00 +0530

    Time: 11 ms | Memory: 13.3 MB - LeetPush

--- 152-maximum-product-subarray/solution.py
+++ 152-maximum-product-subarray/solution.py
@@ -1,20 +1,7 @@
 # Maximum Product Subarray
 # Difficulty: Medium
-# Runtime: 15 ms
-# Memory: 13.1 MB
+# Runtime: 11 ms
+# Memory: 13.3 MB
 # https://leetcode.com/problems/maximum-product-subarray/
 
-        curr_max = nums[0]
-        curr_min = nums[0]
-        ans= nums[0]
-
-        for i in range(1,n):
-            temp_max = max(nums[i],curr_max*nums[i],curr_min*nums[i])
-            temp_min = min(nums[i],curr_max*nums[i],curr_min*nums[i])
-
-            curr_max = temp_max
-            curr_min = temp_min
-        
-            ans = max(curr_max, ans)
-
-        return ans
+ 
