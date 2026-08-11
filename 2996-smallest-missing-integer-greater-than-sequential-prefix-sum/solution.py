a7442c58269a8163c9cd5ae483de5e5343a3d97c
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-08-11 19:48:07 +0530

    Time: 2 ms | Memory: 12.3 MB - LeetPush

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/solution.py
@@ -0,0 +1,7 @@
+# Smallest Missing Integer Greater Than Sequential Prefix Sum
+# Difficulty: Easy
+# Runtime: 2 ms
+# Memory: 12.3 MB
+# https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+
+c
