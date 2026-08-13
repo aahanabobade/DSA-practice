16194466a922168e24378b172b55c68932070a1e
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-08-13 20:16:59 +0530

    Time: 5805 ms | Memory: 69.3 MB - LeetPush

---
+++ 2213-longest-substring-of-one-repeating-character/solution.py
@@ -0,0 +1,25 @@
+# Longest Substring of One Repeating Character
+# Difficulty: Hard
+# Runtime: 5805 ms
+# Memory: 69.3 MB
+# https://leetcode.com/problems/longest-substring-of-one-repeating-character/
+
+            suffix = b[3]
+
+            prefix = a[2]
+            left_char = a[0]
+            right_char = b[1]
+                return a
+
+                return b
+            if b is None:
+            if a is None:
+
+        def merge(a, b):
+        tree = [None] * (4 * n)
+        # Each node:
+        # [left_char, right_char, prefix, suffix, best, length]
+    def longestRepeating(self, s, queryCharacters, queryIndices):
+        n = len(s)
+
+class Solution:
