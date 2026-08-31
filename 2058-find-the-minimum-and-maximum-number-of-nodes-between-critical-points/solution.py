f74551b0371654f20560c01d440befc61d44ffac
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-08-31 11:12:27 +0530

    Time: 199 ms | Memory: 90.6 MB - LeetPush

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.py
@@ -0,0 +1,7 @@
+# Find the Minimum and Maximum Number of Nodes Between Critical Points
+# Difficulty: Medium
+# Runtime: 199 ms
+# Memory: 90.6 MB
+# https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/
+
+#
