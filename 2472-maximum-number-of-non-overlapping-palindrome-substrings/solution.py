e7afcf1bfb163fc985161bcbfe2bf7afc0e6eb90
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-09-15 15:25:59 +0530

    Time: 3533 ms | Memory: 43.6 MB - LeetPush

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/solution.py
@@ -0,0 +1,7 @@
+# Maximum Number of Non-overlapping Palindrome Substrings
+# Difficulty: Hard
+# Runtime: 3533 ms
+# Memory: 43.6 MB
+# https://leetcode.com/problems/maximum-number-of-non-overlapping-palindrome-substrings/
+
+c
