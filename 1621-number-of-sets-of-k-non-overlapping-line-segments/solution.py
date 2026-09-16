33beecc70835d5510cd9a9627167e0c50afc473f
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-09-16 13:17:10 +0530

    Time: 647 ms | Memory: 52.6 MB - LeetPush

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/solution.py
@@ -0,0 +1,7 @@
+# Number of Sets of K Non-Overlapping Line Segments
+# Difficulty: Medium
+# Runtime: 647 ms
+# Memory: 52.6 MB
+# https://leetcode.com/problems/number-of-sets-of-k-non-overlapping-line-segments/
+
+c
