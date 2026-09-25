fabbd2b326e4d997972e978b56c441da69ff46a5
Aahana Bobade <121156155+aahanabobade@users.noreply.github.com>
2026-09-25 11:50:39 +0530

    Time: 3 ms | Memory: 12.6 MB - LeetPush

---
+++ 1096-brace-expansion-ii/solution.py
@@ -0,0 +1,15 @@
+# Brace Expansion II
+# Difficulty: Hard
+# Runtime: 3 ms
+# Memory: 12.6 MB
+# https://leetcode.com/problems/brace-expansion-ii/
+
+                i += 1
+
+            return ans, i
+
+        result, _ = parse(0)
+
+        return sorted(result)
+
+            if i < n and expression[i] == '}':
